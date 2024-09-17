5c924a56f478733b418236e28a70d23b54f8f1fe
Zedhugh Chen <zedhugh@gmail.com>
2024-09-17 22:34:03 +0800

    define the type of a new worker cache item

--- js/worker.d.ts
+++ js/worker.d.ts
@@ -14,3 +14,10 @@ export interface WorkerConfig {
   root: string;
   config: string;
 }
+
+export interface ESLintWorker {
+  root: string;
+  config: string;
+  worker: Worker;
+  files: Set<string>;
+}
